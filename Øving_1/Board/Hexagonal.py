8b2221da941e461e911d6a55d9cb1662358bedb1
Agnar22 <agnar.bjornstad@gmail.com>
2020-01-15 14:32:54 +0100

    Create board øving 1

---
+++ Øving_1/Board/Hexagonal.py
@@ -0,0 +1,38 @@
+import numpy as np
+import json
+
+
+class Hexagonal:
+    def __init__(self):
+        self.neighbours = [(-1, -1), (-1, 0), (0, -1), (0, 1), (1, 0), (1, 1)]
+        self.data = {}
+        with open('../PivotalParameters.json') as json_file:
+            self.data = json.load(json_file)
+        self.board = self._create_board(self.data['board_type'], self.data['board_size'], self.data['cell_types'])
+
+    @staticmethod
+    def _create_board(board_type, dimensions, cell_types):
+        '''
+
+        @param type:
+        @param dimensions:
+        @param cell_types:
+        @return:
+        '''
+
+        board = np.array([])
+        if board_type == 'triangle':
+            board = np.array([[0 if x <= y else -1 for x in range(dimensions)] for y in range(dimensions)])
+        elif board_type == 'diamond':
+            board = np.array(
+                [[0 if (x <= y and y < dimensions) or (x <= 2 * dimensions - y - 2 and y >= dimensions) else -1 for x in
+                  range(dimensions)] for y in range(2 * dimensions - 1)])
+        
+        for num, cell_type in enumerate(cell_types):
+            for y, x in cell_type:
+                board[y, x] = num + 1 if board[y, x] != -1 else -1
+        return board
+
+
+if __name__ == '__main__':
+    print(Hexagonal().board)

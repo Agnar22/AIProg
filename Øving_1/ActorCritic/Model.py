37e2d95c34784d1899eb9f84b407413f7cf8dfb0
Agnar22 <agnar.bjornstad@gmail.com>
2020-02-11 16:57:48 +0100

    Eligibility traces are working

--- Øving_1/ActorCritic/Model.py
+++ Øving_1/ActorCritic/Model.py
@@ -7,16 +7,16 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(2, 10)
+        self.fc1 = nn.Linear(2, 1)
         # self.fc2 = nn.Linear(100, 100)
         # self.fc3 = nn.Linear(100, 100)
-        self.fc4 = nn.Linear(10, 1)
+        # self.fc4 = nn.Linear(10, 1)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
+        # x = F.sigmoid(self.fc1(x))
         # x = F.relu(self.fc2(x))
         # x = F.relu(self.fc3(x))
-        x = F.sigmoid(self.fc4(x))
+        x = F.sigmoid(self.fc1(x))
         return x
 
 
@@ -40,10 +40,9 @@ if __name__ == '__main__':
     dataset_x = data.TensorDataset(tensor_x, tensor_y)
     dataloader = data.DataLoader(dataset_x)
 
-    elig = [torch.Tensor(np.zeros((10, 2))),
-            torch.Tensor(np.zeros((1, 10)))]
+    elig = [torch.Tensor(np.zeros((1, 2))), torch.Tensor(np.zeros((1)))]
 
-    for epoch in range(100):
+    for epoch in range(2000):
         running_loss = 0.0
 
         for i, data in enumerate(dataloader, 0):
@@ -51,22 +50,14 @@ if __name__ == '__main__':
             optimizer.zero_grad()
 
             outputs = net(inputs)
-            # print(outputs, inputs, labels)
             loss = criterion(outputs, labels)
             loss.backward()
-            td_error = labels - outputs
+            td_error = outputs - labels
             for num, f in enumerate(net.parameters()):
-                if num % 2 == 1: continue
-                # print(num, f.grad.shape, elig[num//2].shape)
-
-                elig[num // 2] = elig[num // 2] - f.grad * ((2 * td_error) ** (-1))
-                print(td_error, elig[num // 2])
-                # print(td_error * elig[num // 2])
-                # f.grad.data = td_error * elig[num // 2]
-                # print(elig[num // 2].mean(), elig[num // 3].abs().mean())
-                # print(elig[num//2])
-                # print(f.grad.shape)
+                elig[num] = elig[num] + f.grad * ((2 * float(td_error)) ** (-1))
+                f.grad = float(td_error) * elig[num]
+                elig[num] = 0.1 * elig[num]
             optimizer.step()
 
             running_loss += loss.item()
-        # print("epoch: {0}, running_loss: {1:.3f}".format(epoch, running_loss / 4))
+        print("epoch: {0}, running_loss: {1:.3f}".format(epoch, running_loss / 4))

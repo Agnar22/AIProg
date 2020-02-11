9556276c60cdfcac26db8f011002345554305499
Agnar22 <agnar.bjornstad@gmail.com>
2020-02-11 19:09:01 +0100

    NN and table in critic to one file

--- Øving_1/ActorCritic/Critic.py
+++ Øving_1/ActorCritic/Critic.py
@@ -1,21 +1,18 @@
-import numpy as np
-
-
 class Critic:
-    # Can use function approximator (ANN) or table based
-    def __init__(self, learning_rate):
+
+    def __init__(self, learning_rate, value_approximator):
         self.state_values = {}
         self.state_eligibilities = {}
         self.lr = learning_rate
+        self.value_approximator = value_approximator
 
     def compute_td_error(self, from_state, to_state, reward, discount_factor):
-        # print(self.state_values.setdefault(from_state, np.random.uniform(-0.1, 0.1)))
-        # print(self.state_values.setdefault(to_state, np.random.uniform(-0.1, 0.1)))
-        #print(reward, - self.state_values.setdefault(from_state, np.random.uniform(-0.01, 0.01)))
-        comp= reward + discount_factor * self.state_values.setdefault(to_state, np.random.uniform(-0.01, 0.01)) \
-               - self.state_values.setdefault(from_state, np.random.uniform(-0.01, 0.01))
-        #print("comp", comp)
-        return comp
+        target_value = reward + discount_factor * self.value_approximator.predict(to_state)
+        # target_value = reward + discount_factor * self.state_values.setdefault(to_state, np.random.uniform(-0.01, 0.01))
+        current_value = self.value_approximator.predict(from_state)
+        # current_value = self.state_values.setdefault(from_state, np.random.uniform(-0.01, 0.01))
+        td_error = target_value - current_value
+        return td_error
 
     def set_eligibility(self, state, eligibility):
         self.state_eligibilities[state] = eligibility
@@ -23,6 +20,6 @@ class Critic:
     def get_eligibility(self, state):
         return self.state_eligibilities[state]
 
-    def update_state_value(self, state, td_error):
-        self.state_values[state] = self.state_values[state] + self.lr * self.state_eligibilities[state] * td_error
-        # print("New state value", self.state_values[state])
+    def update_state_value(self, state, td_error, target):
+        # self.state_values[state] = self.state_values[state] + self.lr * self.state_eligibilities[state] * td_error
+        self.value_approximator.fit(state, td_error, target)
--- Øving_1/ActorCritic/CriticModules.py
+++ Øving_1/ActorCritic/CriticModules.py
@@ -2,21 +2,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# Use MSE
+class Table:
+    def __init__(self):
+        self.state_values = dict()
+
+    def predict(self, state):
+        return self.state_values.setdefault(state, np.random.uniform(-0.01, 0.01))
+
+
+class NNApproximator:
+    def __init__(self, NN):
+        self.NN = NN
+
+    def predict(self, x):
+        return self.NN(torch.Tensor(x))
+
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(2, 1)
-        # self.fc2 = nn.Linear(100, 100)
-        # self.fc3 = nn.Linear(100, 100)
-        # self.fc4 = nn.Linear(10, 1)
+        self.fc1 = nn.Linear(2, 100)
+        self.fc2 = nn.Linear(100, 100)
+        self.fc3 = nn.Linear(100, 100)
+        self.fc4 = nn.Linear(100, 1)
 
     def forward(self, x):
-        # x = F.sigmoid(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc3(x))
-        x = F.sigmoid(self.fc1(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.sigmoid(self.fc4(x))
         return x
 
 
@@ -27,6 +41,7 @@ if __name__ == '__main__':
     from torch.utils import data
 
     net = Net()
+    # net.predict([np.array([1, 0])])
 
     criterion = nn.MSELoss()
     optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.0)
@@ -40,7 +55,12 @@ if __name__ == '__main__':
     dataset_x = data.TensorDataset(tensor_x, tensor_y)
     dataloader = data.DataLoader(dataset_x)
 
-    elig = [torch.Tensor(np.zeros((1, 2))), torch.Tensor(np.zeros((1)))]
+    elig = [
+        torch.Tensor(np.zeros((100, 2))), torch.Tensor(np.zeros((100))),
+        torch.Tensor(np.zeros((100, 100))), torch.Tensor(np.zeros((100))),
+        torch.Tensor(np.zeros((100, 100))), torch.Tensor(np.zeros((100))),
+        torch.Tensor(np.zeros((1, 100))), torch.Tensor(np.zeros((1)))
+    ]
 
     for epoch in range(2000):
         running_loss = 0.0
@@ -56,7 +76,7 @@ if __name__ == '__main__':
             for num, f in enumerate(net.parameters()):
                 elig[num] = elig[num] + f.grad * ((2 * float(td_error)) ** (-1))
                 f.grad = float(td_error) * elig[num]
-                elig[num] = 0.1 * elig[num]
+                elig[num] = 0.6 * elig[num]
             optimizer.step()
 
             running_loss += loss.item()

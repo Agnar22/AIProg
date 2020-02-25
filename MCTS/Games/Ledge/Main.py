2c37e534118a7d972ccd2916c548d7a3e1369e06
Agnar22 <agnar.bjornstad@gmail.com>
2020-02-25 14:27:46 +0100

    Finished last methods

--- MCTS/Games/Ledge/Main.py
+++ MCTS/Games/Ledge/Main.py
@@ -5,8 +5,22 @@ class Ledge:
     def __init__(self, board_str):
         self.board_start = board_str
         self.board = np.array([int(x) for x in board_str])
+        self.store_board = None
         self.history = []
+        self.store_history = None
         self.turn = 0
+
+    def store_state(self):
+        self.store_board = np.array(self.board)
+        self.store_history = list(self.history)
+
+    def load_state(self):
+        self.board = np.array(self.store_board)
+        self.history = list(self.store_history)
+
+    def get_turn(self):
+        assert (self.turn == len(self.history) % 2)
+        return self.turn
 
     def get_state(self):
         return (str(self.history), np.array(self.board))
@@ -29,28 +43,32 @@ class Ledge:
     def execute_move(self, move):
         if '0' == move:
             self.board[0] = 0
-            self.history.append(0)
+            self.history.append([0, np.array(self.board)])
             self.turn = len(self.history) % 2
             return None
 
         pos_from, pos_to = map(lambda x: int(x), move.split('_'))
 
         assert (pos_from > pos_to >= 0)
+        # print(pos_from, pos_to, self.board[pos_from], self.board[pos_to], self.board)
         assert (self.board[pos_from] > 0 and self.board[pos_to] == 0)
         # TODO: check squares in between
 
         self.board[pos_to] = self.board[pos_from]
         self.board[pos_from] = 0
-        self.history.append(move)
+        self.history.append([move, np.array(self.board)])
         self.turn = len(self.history) % 2
 
     def undo_move(self):
-        pass
+        # print(self.history)
+        self.board = np.array(self.history.pop()[1])
+        self.turn = len(self.history) % 2
 
     def is_finished(self):
         return True if (self.board == 2).sum() == 0 else False
 
     def outcome(self):
+        # print([1, -1] if self.turn == 1 else [-1, 1])
         return [1, -1] if self.turn == 1 else [-1, 1]
 
     def reset(self):

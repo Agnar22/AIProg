3668d6eb92f23981fd78cc36c0bb8604e57556da
Agnar22 <agnar.bjornstad@gmail.com>
2020-02-25 12:46:44 +0100

    Finish MCTS

--- MCTS/MCTS/Main.py
+++ MCTS/MCTS/Main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import random
-
-
-# from multiprocessing import
+import math
 
 
 class MCTS:
@@ -18,19 +16,16 @@ class MCTS:
             game.load_state()
 
     def _single_search(self, game):
+        if game.is_final(): return game.outcome
+
         # Tree search
-        state = game.get_state()
+        state = game.get_state()[0]
         if len(self.states[state][1]) > 0:
-            move = self._tree_search(game)
-            game.execute_move(move)
-            outcome = self._single_search(game)
-            game.undo_move()
+            outcome = self._tree_search(game, state)
+            return outcome
 
         # Node expansion
-        moves = game.get_moves()
-        self.states[state][1] = moves
-        for action in moves:
-            self.state_action[state + '_' + action] = [0, 0]
+        self._node_expansion(game, state)
 
         # Leaf evaluation
         evaluation = self.rollout(game)
@@ -38,25 +33,64 @@ class MCTS:
         # Backpropagation
         return evaluation
 
-    def _tree_search(self, game):
-        pass
+    def _tree_search(self, game, state):
+        move = self._select_move(game, state)
+        game.execute_move(move)
+        outcome = self._single_search(game)
+        game.undo_move()
 
-    def _node_expansion(self):
-        pass
+        self.states[state][0] = self.states[state][0] + 1
+        move_statistics = self.state_action[state + '_' + move]
+        move_statistics[0] += 1
+        move_statistics[1] += outcome[game.get_turn()]
+        self.state_action[state + '_' + move] = move_statistics
+        return outcome
+
+    def _select_move(self, game, state):
+        max_val = None
+        max_action = None
+
+        for action in game.get_moves():
+            score = MCTS.uct(1, self.state[state], self.state_action[state + '_' + action])
+            if score == None: return action
+            if score > max_val:
+                max_val = score
+                max_action = action
+        return max_action
+
+    def _node_expansion(self, game, state):
+        moves = game.get_moves()
+        self.states[state][1] = moves
+        for action in moves:
+            self.state_action[state + '_' + action] = [0, 0]
 
     def rollout(self, game):
+        move_count = 0
         while not game.is_finished():
             moves = game.get_moves()
             game.execute_move(moves[random.randint(len(moves))])
+            move_count += 1
+
+        for _ in range(move_count):
+            game.undo_move()
         return game.outcome()
 
     def backpropagation(self):
         pass
 
     @staticmethod
-    def uct():
-        pass
+    def uct(c, parent, child):
+        if child[0] == 0: return None
+
+        exploration = c * math.sqrt(math.log(parent[0]) / child[0])
+        q_value = child[1] / child[0]
+        return q_value + exploration
 
     def reset(self):
         self.states = {}
         self.state_action = {}
+
+
+if __name__ == '__main__':
+    pass
+
